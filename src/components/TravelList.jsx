122beb5189442e67f89bc62b159719d3f4df903d
Rafa <rafaokc33@gmail.com>
2024-05-02 21:16:49 +0200

    Iteration 2

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -11,9 +11,22 @@ function TravelList() {
           <div className="Card" key={el.id}>
             <img id="cardImg" src={el.image} />
             <section id="cardSection">
-            <h2>{el.destination} ({el.days} days)</h2>
-            <p>{el.description}</p>
-            <p>Price: {el.totalCost}</p>
+              <h2>{el.destination} ({el.days} days)</h2>
+              <p>{el.description}</p>
+              <div id="price">
+                <h4>Price:</h4><p>{el.totalCost}€</p>
+              </div>
+              <div id="labels">
+                <p className={el.totalCost >= 1500 ? "premium" : ""}>
+                  {el.totalCost >= 1500 ? "Premium" : null}
+                </p>
+                <p className={el.totalCost <= 350 ? "deal" : ""}>
+                  {el.totalCost <= 350 ? "Great Deal" : null}
+                </p>
+                <p className={el.parts[0].name === "All-Inclusive Package" ? "all-inclusive" : ""}>
+                  {el.parts[0].name === "All-Inclusive Package" ? "All-inclusive" : null}
+                </p>
+              </div>
             </section>
           </div>
         )
